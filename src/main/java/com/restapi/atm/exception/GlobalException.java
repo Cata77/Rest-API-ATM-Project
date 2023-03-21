d88dd4d4e93231048b50edd2a2f5e73cc618db85
Buzgau Catalin <buzgaucatalin@yahoo.com>
2023-03-21 17:40:39 +0200

    Refactor exception class

--- src/main/java/com/restapi/atm/exception/GlobalException.java
+++ src/main/java/com/restapi/atm/exception/GlobalException.java
@@ -2,32 +2,31 @@ package com.restapi.atm.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalException {
 
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<Object> handlerUserAlreadyExists(UserAlreadyExistsException exception, WebRequest request) {
+    public ResponseEntity<Object> handlerUserAlreadyExists(UserAlreadyExistsException exception) {
         return new ResponseEntity<>(new ApiError(exception.getMessage(), HttpStatus.NOT_FOUND, LocalDateTime.now()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Object> handlerUserNotFound(UserNotFoundException exception, WebRequest request) {
+    public ResponseEntity<Object> handlerUserNotFound(UserNotFoundException exception) {
         return new ResponseEntity<>(new ApiError(exception.getMessage(), HttpStatus.NOT_FOUND, LocalDateTime.now()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(LowBalanceException.class)
-    public ResponseEntity<Object> handlerLowBalance(LowBalanceException exception, WebRequest request) {
+    public ResponseEntity<Object> handlerLowBalance(LowBalanceException exception) {
         return new ResponseEntity<>(new ApiError(exception.getMessage(), HttpStatus.NOT_ACCEPTABLE, LocalDateTime.now()), HttpStatus.NOT_ACCEPTABLE);
     }
 
     @ExceptionHandler(UserTransactionsNotFoundException.class)
-    public ResponseEntity<Object> handlerTransactionsNotFound(UserTransactionsNotFoundException exception, WebRequest request) {
+    public ResponseEntity<Object> handlerTransactionsNotFound(UserTransactionsNotFoundException exception) {
         return new ResponseEntity<>(new ApiError(exception.getMessage(), HttpStatus.NOT_FOUND, LocalDateTime.now()), HttpStatus.NOT_FOUND);
     }
 }
